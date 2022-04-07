6c3fa44b40bfc3299f303caebcc540b53edfd19a
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-07 00:25:43 +0000

    [Add]ActiveStorageをインストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "01bccec9d730403caaa9eec2d81a9230.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
 
