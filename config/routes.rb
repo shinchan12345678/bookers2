aae886cb7e91c700d4ce72b3d92df1d5441c8a06
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-09 04:48:02 +0000

    [Add]bookの投稿機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :books
-  ##質問
-  # resources :users,only:[:index,:show]
-  resources :users,only:[:index]
   devise_for :users
+  ##質問
+  resources :users,only:[:index,:show]
+  # resources :users,only:[:index]
   root to: 'homes#top'
   get 'homes/about' => "homes#about",as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
