10b4c4eafca4cadb236888653829954840cc20a7
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-09 08:03:56 +0000

    [Update]books_indexページの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
-
   has_many :books, dependent: :destroy
 
+  #railsで画像を処理するため、テーブルに画像を保存する必要がある
+  def get_image
+    unless image.attached?
+      file_path=Rails.root.join('app/assets/images/no_image.jpeg')
+      image.attach(io: File.open(file_path),filename: "no_image.jpeg",content_type: 'image/jpeg')
+    end
+    image
+  end
 end
