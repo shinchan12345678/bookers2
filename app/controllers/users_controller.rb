b50ab182e5294ff96e6c283df66f2a362aafee74
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-09 01:11:59 +0000

    [Add]ログイン認証に使用するキー項目を変更した（email -> name)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,4 @@ class UsersController < ApplicationController
   def show
   end
 
-  def edit
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :books
+  ##質問
+  # resources :users,only:[:index,:show]
+  resources :users,only:[:index]
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about'
+  get 'homes/about' => "homes#about",as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
