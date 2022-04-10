7cbd16682915d4ab58d97665ec799edfddc1b4b8
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-10 06:52:31 +0000

    [Add]bookモデルの削除機能と更新機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,15 +18,21 @@ class BooksController < ApplicationController
     @user=User.find(@book.user_id)
   end
 
-  
+
   def edit
-    
+    @book=Book.find(params[:id])
   end
-  
+
   def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
-  
+
   def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
