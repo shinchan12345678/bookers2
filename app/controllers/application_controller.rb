b60a9bf3006296b76090ad29ce7cc52cf08406fc
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-11 12:48:53 +0000

    [Fix]ActiveStorageでuserモデルに設定したカラム名を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
+# 　bundle exec rspec spec/ --format documentation -o rspec.log
   before_action :authenticate_user!, except: [:top,:about]
+  # before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters,if: :devise_controller?
 
+  
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
@@ -10,12 +13,11 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  protected
+protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys: [:email])
     # devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
     devise_parameter_sanitizer.permit(:sign_in,keys: [:name])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :image
+  has_one_attached :profile_image
   has_many :books, dependent: :destroy
-  
-  validates :name,presence: true
+
+  validates :name,presence: true, uniqueness: true,length: {minimum: 2,maximum: 20}
+  validates :introduction,length: {maximum: 50}
 
   #railsで画像を処理するため、テーブルに画像を保存する必要がある
-  def get_image
-    unless image.attached?
-      file_path=Rails.root.join('app/assets/images/no_image.jpeg')
-      image.attach(io: File.open(file_path),filename: "no_image.jpeg",content_type: 'image/jpeg')
+#   def get_image
+#     unless image.attached?
+#       file_path=Rails.root.join('app/assets/images/no_image.jpeg')
+#       image.attach(io: File.open(file_path),filename: "no_image.jpeg",content_type: 'image/jpeg')
+#     end
+#     image
+#   end
+# end
+
+  def get_profile_image
+    unless self.profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    self.profile_image.variant(resize_to_limit: [100,100]).processed
   end
 end
